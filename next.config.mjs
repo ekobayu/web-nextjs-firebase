70d123061edf656770ad58ca454c42a837354946
Eko Bayu <eko.bayu.seo@gmail.com>
2024-09-13 21:27:50 +0800

    setup sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // basePath: '/ekobayu-profile.github.io/',
+  // output: 'export',
+  sassOptions: {
+    includePaths: ['/'],
+    prependData: `@import "/styles/variables.scss"; @import "/styles/mixin.scss";`
+  }
+  // clientRouterFilter: false
+  // swcMinify: false,
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
 
-export default nextConfig;
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+export default nextConfig
